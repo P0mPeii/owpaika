e1d8616d7ccd6d9b822b9137e684f05fcb294203
P0mPeii <110507069+P0mPeii@users.noreply.github.com>
2025-02-14 00:54:35 +0800

    redis序列化

---
+++ owpaicard-server/src/main/java/com/owpai/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.owpai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        // 设置连接工厂
+        redisTemplate.setConnectionFactory(connectionFactory);
+        // 设置key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // 设置value的序列化器
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        // 设置hash key的序列化器
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        // 设置hash value的序列化器
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
