f8963607f200199cd8d9b0db45278a1e603fcce1
P0mPeii <110507069+P0mPeii@users.noreply.github.com>
2025-02-13 23:51:12 +0800

    swagger,OrderStatus实现

---
+++ owpaicard-pojo/src/main/java/com/owpai/pojo/enums/OrderStatus.java
@@ -0,0 +1,38 @@
+package com.owpai.pojo.enums;
+
+public enum OrderStatus {
+    /**
+     * 待支付
+     */
+    PENDING,
+
+    /**
+     * 已支付
+     */
+    PAID,
+
+    /**
+     * 已完成
+     */
+    SENT,
+
+    /**
+     * 用户/admin已取消
+     */
+    CANCEL,
+
+    /**
+     * 超时
+     */
+    TIMEOUT,
+
+    /**
+     * 已退款
+     */
+    REFUND,
+
+    /**
+     * 拒绝退款
+     */
+    REFUSE_REFUND
+}
