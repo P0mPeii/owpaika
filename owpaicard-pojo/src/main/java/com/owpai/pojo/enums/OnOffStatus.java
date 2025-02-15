6ffc9d65d08a8e751f6ec19cc702f54476f32aec
P0mPeii <110507069+P0mPeii@users.noreply.github.com>
2025-02-16 00:07:26 +0800

    refactor(pojos): 重构上下架状态枚举
    - 修改枚举值的定义方式，使用 code 和 description 字段替代 mask 字段
    - 更新枚举值的含义和用途：
      - ALL: 表示全部上下架状态  - ON: 表示已上架状态
      - OFF: 表示已下架状态- 添加 description 字段用于描述每个枚举值的含义

--- owpaicard-pojo/src/main/java/com/owpai/pojo/enums/OnOffStatus.java
+++ owpaicard-pojo/src/main/java/com/owpai/pojo/enums/OnOffStatus.java
@@ -5,20 +5,16 @@ import lombok.Getter;
 @Getter
 public enum OnOffStatus {
 
-    ALL(1),
+    OFF(0, "已下架"),
 
-    ON(1 << 1),
+    ON(1, "已下架"),
 
-    OFF(1 << 2);
+    ALL(99, "全部上下架状态");
+    private final int code;
+    private final String description;
 
-    private final int mask;
-
-    OnOffStatus(int mask) {
-        this.mask = mask;
+    OnOffStatus(final int code, final String description) {
+        this.code = code;
+        this.description = description;
     }
-
-    public int getMask() {
-        return mask;
-    }
-
 }
